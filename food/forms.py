39c5a439bdda07ca284c866a53d31ffc35de00e4
Elisabeth Volke <elli.volke@web.de>
2026-01-22 10:22:36 +0100

    ...

--- food/forms.py
+++ food/forms.py
@@ -6,9 +6,6 @@ from food.models import Food, Ingredient, Recipe
 
 class AddRecipeForm(ModelForm):
 
-    # name = CharField(required=True)
-    # instructions = CharField(required=False, widget=Textarea)
-
     class Meta:
         model = Recipe
         fields = "__all__"
@@ -17,7 +14,6 @@ class AddIngredientForm(ModelForm):
     food = ModelChoiceField(queryset=Food.objects.all(), 
                                     widget=autocomplete.ModelSelect2(url="food-autocomplete"),
                                     required=False)
-    # amount = IntegerField(required=False)
 
     class Meta:
         model = Ingredient
