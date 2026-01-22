3d278a28b78ec350c4e8441b9cd4f8a0629e68f2
Elisabeth Volke <elli.volke@web.de>
2026-01-22 10:21:50 +0100

    links to recipe_list and change_recipe

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
         views.FoodAutocomplete.as_view(),
         name='food-autocomplete',
     ),
+    path("recipe/add", views.add_change_recipe, name="add_recipe"),
+    path("recipe/<path:recipe_id>/change", views.add_change_recipe, name="change_recipe"),
     path("recipe/", views.recipe_list, name="recipe_list"),
 ]
